54f030958bd49c9a92c0d9ed04ec5738d1de094a
yangyulin <yyl815@163.com>
2019-05-31 15:48:16 +0800

    optimized code

--- main/main.js
+++ main/main.js
@@ -1,16 +1,16 @@
 function price(x,fees,m){
-	if(x<2){}
+	if(x<=2){}
 	else {
-		fees=fees+(x-2)*0.8;
+		fees+=(x-2)*0.8;
 		if(x>8){
-			fees=fees+(x-8)*0.4;
+			fees+=(x-8)*0.4;
 		}
 	}
 	return Math.round(fees+m*0.25);
 }
 
-module.exports = function main(x,m) {
+module.exports = function main(input) {
     var fees=6;
-    fees=price(x,fees,m);
+    fees=price(input.distance,fees,input.parkTime);
     return fees;
 };
