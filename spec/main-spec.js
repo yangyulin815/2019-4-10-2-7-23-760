d37ce05dc6f9e037259bbfbb5bfde1d8adf48804
yangyulin <yyl815@163.com>
2019-05-31 11:44:16 +0800

    more than 8km and parking test

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -19,13 +19,13 @@ describe('taxi fee', function () {
         let expected = 13;
         expect(summary).toEqual(expected);
     });
-/*    it('more than 8km and parking' , function() {
+   it('more than 8km and parking' , function() {
         let inputs = {distance:10,parkTime:20};
-        let summary = main(inputs);
+        let summary = main(inputs.distance,inputs.parkTime);
         let expected = 18;
         expect(summary).toEqual(expected);
     });
-    it('more than 8km and no parking' , function() {
+/*    it('more than 8km and no parking' , function() {
         let inputs = {distance:15,parkTime:0};
         let summary = main(inputs);
         let expected = 19;
