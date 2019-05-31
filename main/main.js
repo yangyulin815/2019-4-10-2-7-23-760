a61236710a75ded974a13e5a2908d949cd24a820
yangyulin <yyl815@163.com>
2019-05-31 11:44:38 +0800

    more than 8km and parking

--- main/main.js
+++ main/main.js
@@ -1,8 +1,11 @@
 module.exports = function main(x,m) {
     var fees;
 	fees=6;
-	if(x>2){
+	if(x>2&&x<=8){
 		fees=fees+(x-2)*0.8;
+	}
+	else if(x>8){
+		fees=fees+(8-2)*0.8+(x-8)*1.2;
 	}
     fees=Math.round(fees+m*0.25);
     return fees;
