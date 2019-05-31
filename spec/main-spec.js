23a340fff84049dc9747bff44496f79cc36db8fd
yangyulin <yyl815@163.com>
2019-05-31 15:48:49 +0800

    optimized code

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -3,43 +3,43 @@ const main = require('../main/main');
 describe('taxi fee', function () {
     it('only within 2km' , function() {
         let inputs = {distance:1,parkTime:0};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 6;
         expect(summary).toEqual(expected);
     });
     it('within 2km and parking' , function() {
         let inputs = {distance:1,parkTime:5};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 7;
         expect(summary).toEqual(expected);
     });
     it('between 2km and 8km and parking' , function() {
         let inputs = {distance:7,parkTime:10};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 13;
         expect(summary).toEqual(expected);
     });
    it('more than 8km and parking' , function() {
         let inputs = {distance:10,parkTime:20};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 18;
         expect(summary).toEqual(expected);
     });
     it('more than 8km and no parking' , function() {
         let inputs = {distance:15,parkTime:0};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 19;
         expect(summary).toEqual(expected);
     });
         it('equal to 2km and no parking' , function() {
         let inputs = {distance:2,parkTime:0};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 6;
         expect(summary).toEqual(expected);
     });
         it('equal to 8km and parking' , function() {
         let inputs = {distance:8,parkTime:10};
-        let summary = main(inputs.distance,inputs.parkTime);
+        let summary = main(inputs);
         let expected = 13;
         expect(summary).toEqual(expected);
     });
